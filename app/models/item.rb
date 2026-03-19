5980f5878f41db5f3b7d0d3b8e15b7efab581c42
Yoshida <61533915+naoto-ysd@users.noreply.github.com>
2026-03-20 02:30:37 +0900

    アイテムとタグの関連付けを実装し、ItemTagおよびTagモデルを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+  has_many :item_tags, dependent: :destroy
+  has_many :tags, through: :item_tags
+
+  def tag_names
+    tags.map(&:name).join(", ")
+  end
+
+  def tag_names=(names_string)
+    tag_list = names_string.to_s.split(",").map(&:strip).reject(&:blank?)
+    self.tags = tag_list.map { |name| Tag.find_or_create_by!(name: name) }
+  end
 end
