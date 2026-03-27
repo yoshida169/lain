ee353c706b0eb333de294222924a9e4f16c2d9eb
Yoshida <61533915+naoto-ysd@users.noreply.github.com>
2026-03-28 01:59:17 +0900

    修正: インポート文のタイプミスを修正

--- app/frontend/router/index.ts
+++ app/frontend/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, cretaeWebHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(),
